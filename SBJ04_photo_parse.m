c389a2b231d0a86ad27b79159b855a68bb95ab91
hoycw <hoycw@berkeley.edu>
2019-07-10 10:19:31 -0700

    outline of SBJ04_photo_parse added

---
+++ SBJ04_photo_parse.m
@@ -0,0 +1,164 @@
+function SBJ04_photo_parse(SBJ, block, plot_it, save_it)
+% INPUTS:
+%   SBJ [str] - uniquely identifies the subject, e.g., 'IR54'
+%   block [int] - index of which block of data should be analyzed
+%   plot_it [0/1] - optional. plot_it = 1 to plot detected events
+%   save_it [0/1] - whether to save output
+
+%% File paths
+if exist('/home/knight/hoycw/','dir');root_dir='/home/knight/hoycw/';ft_dir=[root_dir 'Apps/fieldtrip/'];
+elseif exist('G:\','dir');root_dir='G:\';ft_dir='C:\Toolbox\fieldtrip\';
+else root_dir='/Volumes/hoycw_clust/';ft_dir='/Users/colinhoy/Code/Apps/fieldtrip/';end
+addpath(fullfile(root_dir,'emodynamics','scripts','utils'));
+
+% Set up SBJ and directory info
+SBJ_vars_cmd = ['run ' fullfile(root_dir,'emodynamics','scripts','SBJ_vars', [SBJ '_vars.m'])];
+eval(SBJ_vars_cmd);
+if numel(SBJ_vars.raw_file)>1
+    block_suffix = strcat('_',SBJ_vars.block_name{block});
+else
+    block_suffix = SBJ_vars.block_name{block};   % should just be ''
+end
+
+evnt_filename = [SBJ_vars.dirs.preproc SBJ '_evnt_clean',block_suffix,'.mat'];
+output_filename = [SBJ_vars.dirs.events SBJ '_trial_info_auto',block_suffix,'.mat'];
+
+%% Determine event onset sample points
+% Load input data
+fprintf('Loading %s\n',evnt_filename);
+load(evnt_filename);
+data_photo = evnt.trial{1};
+data_photo_orig = data_photo;
+
+% Bring data down to zero
+data_photo = data_photo - min(data_photo);
+
+% Read photodiode data
+fprintf('\tReading photodiode data\n');
+min_event_length = 0.8 * evnt.fsample;    %trial must be at least 0.8 sec (actually ~1.5s?)
+[evnt_on, evnt_off, data_shades] = read_photodiode(data_photo, min_event_length, 2);  %2 different shades (bsln, evnt)
+if save_it
+    fig_fname = [SBJ_vars.dirs.events SBJ '_photo_segmentation.fig'];
+    saveas(gcf,fig_fname);
+end
+clear data_photo;
+
+% Diff to get edges which correspond to onsets and offsets
+data_shades = [diff(data_shades) 0]; % Add a point because diff removes one
+video_onsets = find(data_shades>0)'; % 1 to 2 is video onset. Transpose to make column vector
+fprintf('\t\tFound %d trials in photodiode channel\n', length(video_onsets));
+
+% Plot photodiode event durations to check consistency
+if plot_it
+    figure;
+    dur = evnt_off{2}-evnt_on{2};
+    plot(dur);
+    ylabel('Photodiode Durations');
+    xlabel('Trial');
+    title(['[min, mean, max] = [' num2str(min(dur)) ',' num2str(mean(dur)) ',' num2str(max(dur)) ']']);
+    if any(dur<60)
+        warning(['WARNING!!! ' num2str(sum(dur<60)) ' photodiode events are less than 60 ms!']);
+        disp(dur(dur<60));
+    end
+    if save_it
+        fig_fname = [SBJ_vars.dirs.events SBJ '_photo_durations.png'];
+        saveas(gcf,fig_fname);
+    end
+end
+
+%% Read in log file
+% Open file
+% fprintf('\tReading log file\n');
+% log_h = fopen([SBJ_vars.dirs.events SBJ '_eventInfo' block_suffix '.txt'], 'r');
+
+% Parse log file
+% file_contents = textscan(log_h, '%f %d', 'Delimiter', ',', 'MultipleDelimsAsOne', 1);
+% trial_info.video_id = file_contents{2};
+% trial_info.log_onset_time = file_contents{1};
+% fprintf('\t\tFound %d trials in log file\n', length(trial_info.video_id));
+
+% Remove trials to ignore
+trial_info.video_id(ignore_trials) = [];
+trial_info.log_onset_time(ignore_trials) = [];
+trial_info.ignore_trials = ignore_trials;
+fprintf('\t\tIgnoring %d trials\n', length(ignore_trials));
+
+% If log and photodiode have different n_trials, plot and error out
+if (length(trial_info.video_id) ~= length(video_onsets))
+    % Plot photodiode data
+    plot_photo = data_photo_orig - min(data_photo_orig);
+    plot_photo = plot_photo / (max(plot_photo)-min(plot_photo));
+    plot_photo = plot_photo + 0.25;
+    figure; hold on;
+    plot(plot_photo, 'k');
+    % Plot video onsets
+    for video_n = 1:length(video_onsets)
+        plot([video_onsets(video_n) video_onsets(video_n)],[1.30 1.40],'r','LineWidth',2);
+        plot([video_onsets(video_n) video_onsets(video_n)],[-0.35 0.35],'r','LineWidth',2);
+    end
+    error('\nNumber of trials in log is different from number of trials found in event channel\n\n');
+end
+
+% % Compare onset differences between photodiode and log times
+% log_times = trial_info.log_onset_time-trial_info.log_onset_time(1);
+% video_times = (video_onsets-video_onsets(1))/evnt.fsample;
+% donsets = log_times-video_times;
+% dphoto = diff(video_onsets/evnt.fsample);
+% dlog   = diff(trial_info.log_onset_time);
+% ddif   = dlog-dphoto;
+% fprintf('\tMax difference in photodiode - log event onsets = %f\n',max(abs(donsets)));
+% fprintf('\tMax difference in photodiode - log event durations = %f\n',max(abs(ddif)));
+
+trial_info.video_onsets = video_onsets;
+
+%% Save results
+if save_it
+    save(output_filename,'-v7.3', 'trial_info');
+end
+
+%% Plot results
+if plot_it
+    % Plot event onsets
+    figure;%('Position', [100 100 1200 800]);
+    hold on;
+    
+    % Plot photodiode data
+    plot_photo = data_photo_orig - min(data_photo_orig);
+    plot_photo = plot_photo / (max(plot_photo)-min(plot_photo));
+    plot_photo = plot_photo + 0.25;
+    plot(evnt.time{1}, plot_photo, 'Color', [0.5 0.8 0.8]);
+    plot([0 evnt.time{1}(end)],[0.25 0.25],'k');
+    
+    % Plot video onsets
+    for video_n = 1:length(trial_info.video_onsets)
+        plot([trial_info.video_onsets(video_n) trial_info.video_onsets(video_n)]/evnt.fsample,[1.30 1.40],'b','LineWidth',2);
+        plot([trial_info.video_onsets(video_n) trial_info.video_onsets(video_n)]/evnt.fsample,[-0.35 0.35],'b','LineWidth',2);
+    end
+    
+    if save_it
+        fig_fname = [SBJ_vars.dirs.events SBJ '_events.fig'];
+        saveas(gcf,fig_fname);
+    end
+    
+    % Plot difference between log and photodiode
+    figure; hold on;
+    subplot(2,1,1);
+    plot(donsets);
+    ylabel('Log-Photo Onsets');
+    xlabel('Video');
+    title(['max(abs(donsets)) = ' num2str(max(abs(donsets)))]);
+    
+    subplot(2,1,2);
+    plot(ddif);
+    ylabel('Log-Photo Durations');
+    xlabel('Video');
+    title(['max(abs(diff(durations))) = ' num2str(max(abs(ddif)))]);
+
+    if save_it
+        fig_fname = [SBJ_vars.dirs.events SBJ '_log_photo_QA.png'];
+        saveas(gcf,fig_fname);
+    end
+end
+
+end
+
