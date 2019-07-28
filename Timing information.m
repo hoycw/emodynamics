aa846135756b50932ec7a206d96f18c8b27121d3
khchen1981 <51494192+khchen1981@users.noreply.github.com>
2019-07-27 22:14:09 -0700

    Create Timing information.m
    get the precise timing of films and events. now only is MM:SS:frame number format....

---
+++ Timing information.m
@@ -0,0 +1,74 @@
+
+% 30 frames per second (from #0 to #29)
+% format: MM:SS:frame number
+% MM = 00-59
+% SS = 00-59
+% frame number = 00-29
+
+
+% 1. Disgust: Roaches (154000 ms)
+%       Baseline:   0:00:00 - 0:31:29
+%       Film:       0:32:00 - 2:02:03
+%       Recovery:   2:02:04 - 2:34:04
+%       Event:      0:47:08
+%       Event:      1:12:07
+%       Event:      1:33:11
+
+% 2. Happy: Modern Times (154000 ms)
+%       Baseline:   0:00:00 - 0:31:29
+%       Film:       0:32:00 - 2:02:01
+%       Recovery:   2:02:02 - 2:34:02
+
+% 3. Fear: Witness (154000 ms)
+%       Baseline:   0:00:00 - 0:31:29
+%       Film:       0:32:00 - 2:02:03
+%       Recovery:   2:02:04 - 2:34:04
+%       Event:      1:18:07 (not clear)
+
+% 4. Neutral: Sticks (154000 ms)
+%       Baseline:   0:00:00 - 0:31:29
+%       Film:       0:32:00 - 2:02:01
+%       Recovery:   2:02:02 - 2:34:02
+%       Event:      0:38:22
+%       Event:      0:45:27
+%       Event:      0:53:03
+%       Event:      1:00:07
+%       Event:      1:06:27
+%       Event:      1:14:01
+%       Event:      1:21:20
+%       Event:      1:29:02
+%       Event:      1:36:22
+%       Event:      1:43:22
+%       Event:      1:50:18
+%       Event:      1:57:10
+
+% 5. Fear: Cujo (154000 ms)
+%       Baseline:   0:00:00 - 0:31:29
+%       Film:       0:32:00 - 2:02:03
+%       Recovery:   2:02:04 - 2:34:04
+%       Event:      1:22:07
+
+% 6. Disgust: Poop Lady (154000 ms)
+%       Baseline:   0:00:00 - 0:31:29
+%       Film:       0:32:00 - 2:02:00
+%       Recovery:   2:02:01 - 2:34:01
+%       Event:      --
+
+% 7. Neutral: ColorBars (154000 ms)
+%       Baseline:   0:00:00 - 0:31:29
+%       Film:       0:32:00 - 2:02:03
+%       Recovery:   2:02:04 - 2:34:04
+%       Event:      0:42:00
+%       Event:      0:52:00
+%       Event:      1:02:02
+%       Event:      1:12:02
+%       Event:      1:22:02
+%       Event:      1:32:02
+%       Event:      1:42:02
+%       Event:      1:52:02
+
+% 8. Happy: Lucy (159000 ms)
+%       Baseline:   0:00:00 - 0:31:29
+%       Film:       0:32:00 - 2:06:20
+%       Recovery:   2:06:21 - 2:38:21
+%       Event:      1:59:04
