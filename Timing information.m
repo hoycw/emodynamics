43b2197c85394f979bdf2a989cc63bb0cbfe25e0
khchen1981 <51494192+khchen1981@users.noreply.github.com>
2019-07-28 21:52:35 -0700

    Update Timing information.m
    now all in second.milliseconds! see the bottom section.

--- Timing information.m
+++ Timing information.m
@@ -72,3 +72,151 @@
 %       Film:       0:32:00 - 2:06:20
 %       Recovery:   2:06:21 - 2:38:21
 %       Event:      1:59:04
+
+
+
+%%
+% format: MM:SS:Millisecond
+% MM = 00-59
+% SS = 00-59
+% Millisecond = 000-999
+
+
+% 1. Disgust: Roaches (154000 ms)
+%       Baseline:   0:00:001 - 0:32:000
+%       Film:       0:32:001 - 2:02:133
+%       Recovery:   2:02:134 - 2:34:167
+%       Event:      0:47:268
+%       Event:      1:12:234
+%       Event:      1:33:368
+
+% 2. Happy: Modern Times (154000 ms)
+%       Baseline:   0:00:001 - 0:32:000
+%       Film:       0:32:001 - 2:02:067
+%       Recovery:   2:02:068 - 2:34:100
+
+% 3. Fear: Witness (154000 ms)
+%       Baseline:   0:00:001 - 0:32:000
+%       Film:       0:32:001 - 2:02:133
+%       Recovery:   2:02:134 - 2:34:167
+%       Event:      1:18:234 (not clear)
+
+% 4. Neutral: Sticks (154000 ms)
+%       Baseline:   0:00:001 - 0:32:000
+%       Film:       0:32:001 - 2:02:067
+%       Recovery:   2:02:068 - 2:34:100
+%       Event:      0:38:734
+%       Event:      0:45:901
+%       Event:      0:53:101
+%       Event:      1:00:234
+%       Event:      1:06:901
+%       Event:      1:14:034
+%       Event:      1:21:668
+%       Event:      1:29:068
+%       Event:      1:36:734
+%       Event:      1:43:734
+%       Event:      1:50:601
+%       Event:      1:57:334
+
+% 5. Fear: Cujo (154000 ms)
+%       Baseline:   0:00:001 - 0:32:000
+%       Film:       0:32:001 - 2:02:133
+%       Recovery:   2:02:134 - 2:34:167
+%       Event:      1:22:234
+
+% 6. Disgust: Poop Lady (154000 ms)
+%       Baseline:   0:00:001 - 0:32:000
+%       Film:       0:32:001 - 2:02:033
+%       Recovery:   2:02:034 - 2:34:067
+%       Event:      --
+
+% 7. Neutral: ColorBars (154000 ms)
+%       Baseline:   0:00:001 - 0:32:000
+%       Film:       0:32:001 - 2:02:133
+%       Recovery:   2:02:134 - 2:34:167
+%       Event:      0:42:001
+%       Event:      0:52:001
+%       Event:      1:02:068
+%       Event:      1:12:068
+%       Event:      1:22:068
+%       Event:      1:32:068
+%       Event:      1:42:068
+%       Event:      1:52:068
+
+% 8. Happy: Lucy (159000 ms)
+%       Baseline:   0:00:001 - 0:32:000
+%       Film:       0:32:001 - 2:06:700
+%       Recovery:   2:06:701 - 2:38:733
+%       Event:      1:59:134
+
+
+%%
+% format: SS.milliseconds
+
+
+% 1. Disgust: Roaches (154000 ms)
+%       Baseline:   0.001 - 32.000
+%       Film:       32.001 - 122.133
+%       Recovery:   122.134 - 154.167
+%       Event:      47.268
+%       Event:      72.234
+%       Event:      93.368
+
+% 2. Happy: Modern Times (154000 ms)
+%       Baseline:   0.001 - 32.000
+%       Film:       32.001 - 122.067
+%       Recovery:   122.068 - 154.100
+
+% 3. Fear: Witness (154000 ms)
+%       Baseline:   0.001 - 32.000
+%       Film:       32.001 - 122.133
+%       Recovery:   122.134 - 154.167
+%       Event:      78.234 (not clear)
+
+% 4. Neutral: Sticks (154000 ms)
+%       Baseline:   0.001 - 32.000
+%       Film:       32.001 - 122.067
+%       Recovery:   122.068 - 154.100
+%       Event:      38.734
+%       Event:      45.901
+%       Event:      53.101
+%       Event:      60.234
+%       Event:      66.901
+%       Event:      74.034
+%       Event:      81.668
+%       Event:      89.068
+%       Event:      96.734
+%       Event:      103.734
+%       Event:      110.601
+%       Event:      117.334
+
+% 5. Fear: Cujo (154000 ms)
+%       Baseline:   0.001 - 32.000
+%       Film:       32.001 - 122.133
+%       Recovery:   122.134 - 154.167
+%       Event:      82.234
+
+% 6. Disgust: Poop Lady (154000 ms)
+%       Baseline:   0.001 - 32.000
+%       Film:       32.001 - 122.033
+%       Recovery:   122.034 - 154.067
+%       Event:      --
+
+% 7. Neutral: ColorBars (154000 ms)
+%       Baseline:   0.001 - 32.000
+%       Film:       32.001 - 122.133
+%       Recovery:   122.134 - 154.167
+%       Event:      42.001
+%       Event:      52.001
+%       Event:      62.068
+%       Event:      72.068
+%       Event:      82.068
+%       Event:      92.068
+%       Event:      102.068
+%       Event:      112.068
+
+% 8. Happy: Lucy (159000 ms)
+%       Baseline:   0.001 - 32.000
+%       Film:       32.001 - 126.700
+%       Recovery:   126.701 - 158.733
+%       Event:      119.134
